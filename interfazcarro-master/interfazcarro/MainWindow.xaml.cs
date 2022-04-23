6251c6d9c4ed2eecd354db5bfa05b5c9ad900be4
primera <eflorianc@miumg.edu.gt>
2022-04-22 22:43:30 -0600

    carro

---
+++ interfazcarro-master/interfazcarro/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using interfazcarro.clase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace interfazcarro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Carro carrito; 
+        public MainWindow()
+        {
+            InitializeComponent();
+            carrito = new Carro("fort", 2022, 250, "mario");
+        }
+
+        private void acelerar_Click(object sender, RoutedEventArgs e)
+        {
+           
+         
+            encendido.Text = carrito.acelerar();
+        }
+
+        private void encender_Click(object sender, RoutedEventArgs e)
+        {
+            
+            encendido.Text = carrito.EncenderMotor();
+        }
+
+        private void frenar_Click(object sender, RoutedEventArgs e)
+        {
+            encendido.Text = carrito.FRENAR();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            encendido.Text = carrito.desacelerar(-25);
+        }
+
+        private void apagar_Click(object sender, RoutedEventArgs e)
+        {
+            encendido.Text =  carrito.apagar();
+        }
+    }
+}
---
+++ interfazcarro-master/interfazcarro/clase/carro.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfazcarro.clase
+{
+    internal class Carro
+    {
+        public string Marca { get; }
+        public int Modelo { get; }
+        public String Color { get; set; }
+
+        public string owner { get; set; }
+
+        private int Encendido = 0;
+        private int velocidad_actual = 0;
+
+        public int MAXVELOCIDAD { get; }
+
+
+
+
+        public Carro(string _marca, int _modelo, string _color, int maxvel)
+        {
+            Marca = _marca;
+            Modelo = _modelo;
+            Color = _color;
+            Encendido = 0;
+            MAXVELOCIDAD = maxvel;
+            this.velocidad_actual = 0;
+        }
+
+        //construir 2do constructor
+        public Carro(String _marca, int _modelo, int maxvel, string _owner)
+        {
+            Marca = _marca;
+            Modelo = _modelo;
+            MAXVELOCIDAD = maxvel;
+            owner = _owner;
+            this.velocidad_actual = 0;
+        }
+
+        public int GetVelocidadActual()
+        {
+            return velocidad_actual;
+        }
+
+
+        public string EncenderMotor()
+        {
+            if (Encendido == 0)
+            {
+
+                Encendido = 1;
+                this.velocidad_actual = 0;
+                return "run run run run, listo para correr";
+            }
+            else
+            {
+                return "Orales!!! ya esta encendido tu carro";
+            }
+        }
+
+
+        public string acelerar()
+        {
+            string mensaje = "";
+            if (Encendido == 0)
+            {
+                mensaje = $"no puedo acelerar con el carro apagado";
+                return mensaje;
+            }
+
+            velocidad_actual += 10;
+            if (velocidad_actual <= MAXVELOCIDAD)
+            {
+                mensaje = $"Vas a {velocidad_actual} KPH";
+            }
+            else
+            {
+                velocidad_actual = MAXVELOCIDAD;
+                mensaje = $"{velocidad_actual}KPH   Aguas rapido y furioso, vas muy rapido y ya llegaste al tope";
+            }
+
+
+            return mensaje;
+
+        }
+
+
+
+
+        public string desacelerar(int CuantosKPH)
+        {
+            string mensaje = "";
+            if (Encendido == 0)
+            {
+                mensaje = $"no puedo acelerar con el carro apagado";
+                return mensaje;
+            }
+
+            velocidad_actual += CuantosKPH;
+            if (velocidad_actual >= 0)
+            {
+                mensaje = $"vas frenando {velocidad_actual} KPH";
+            }
+            else
+            {
+                velocidad_actual = 0;
+                mensaje = $"{velocidad_actual}KPH   ya frenaste";
+            }
+
+
+            return mensaje;
+
+        }
+
+
+        public string FRENAR()
+        {
+            string mensaje = "";
+            if (Encendido == 0)
+            {
+                mensaje = $"no puedo acelerar con el carro apagado";
+                return mensaje;
+            }
+
+
+
+            velocidad_actual -= 20;
+            if (velocidad_actual >= 0)
+            {
+                mensaje = $"Vas frenando {velocidad_actual} KPH";
+            }
+            else
+            {
+                mensaje = $"ya freno";
+                return mensaje;
+            }
+
+            Console.WriteLine(mensaje);
+            return mensaje;
+        }
+        public string apagar()
+        {
+            if (Encendido == 1)
+            {
+
+                Encendido = 0;
+                this.velocidad_actual = 0;
+                return "apagar carro";
+            }
+            else
+            {
+                return "el carro ya estaba apagado";
+            }
+        }
+
+
+
+    }
+}
+
